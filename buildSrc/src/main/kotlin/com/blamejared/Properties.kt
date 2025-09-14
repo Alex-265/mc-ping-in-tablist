ce0050720209f324c3f95d05c087f0c65364d619
Alex <system@rundordner.com>
2025-09-14 19:05:02 +0200

    Use Kotlin DSL + Add automated publishing to modrinth and curseforge

---
+++ buildSrc/src/main/kotlin/com/blamejared/Properties.kt
@@ -0,0 +1,15 @@
+package com.blamejared
+
+object Properties {
+    const val GROUP = "at.alex.pingintablist"
+    const val NAME = "PingInTablist"
+    const val AUTHOR = "Alex_265"
+    const val MODID = "pingintablist"
+    const val AVATAR = "https://media.forgecdn.net/avatars/84/193/636193647832736518.gif"
+    const val CURSE_PROJECT_ID = "1063994"
+    const val CURSE_HOMEPAGE = "https://www.curseforge.com/minecraft/mc-mods/ping-in-tablist"
+    const val MODRINTH_PROJECT_ID = "l92I91s6"
+    const val FIRST_COMMIT = "5b21b0a082650c88fc3a2f738ab510268cbdbb62"
+    const val GIT_REPO = "https://github.com/Alex-265/mc-ping-in-tablist"
+    const val DESCRIPTION = "Displays the ping of players in text instead of an icon."
+}
--- common/src/main/java/at/alex/pingintablist/config/ConfigScreen.java
+++ common/src/main/java/at/alex/pingintablist/config/ConfigScreen.java
@@ -7,8 +7,7 @@ import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.StringWidget;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public class ConfigScreen extends Screen {
     private final Screen oldScreen;
